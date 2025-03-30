704b44d04d5a75ccfca6e7dde4cdbf75ee742438
SidneiJunior21 <sidnei.junior@dcomp.ufs.br>
2025-03-30 13:09:02 -0300

    Criação do menu de pause
    menu mais feio q esse impossivelkk

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    public Transform pause;
+    void Start(){
+        Cursor.visible = false;
+    }
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(pause.gameObject.activeSelf)
+            {
+                pause.gameObject.SetActive(false);
+                Time.timeScale = 1;
+                Cursor.visible = false;
+            }
+            else
+            {
+                pause.gameObject.SetActive(true);
+                Time.timeScale = 0;
+                Cursor.visible = true;
+            };
+        }
+    }
+
+    public void LoadScenes(string cena)
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(cena);
+    }
+
+    public void resume()
+    {
+        pause.gameObject.SetActive(false);
+            Time.timeScale = 1; 
+            Cursor.visible = false;
+    }
+}
