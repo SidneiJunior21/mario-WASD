35849b9adacccd62807e2afd0d6f0f0601bf9f67
SidneiJunior21 <sidnei.junior@dcomp.ufs.br>
2025-04-12 15:45:03 -0300

    Criação do menu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+
+    void Start(){
+        Cursor.visible = true;
+    }
+
+    public void LoadScenes(string cena){
+        SceneManager.LoadScene(cena);
+    }
+
+    public void Sair(){
+        Application.Quit();
+    }
+
+    
+}
